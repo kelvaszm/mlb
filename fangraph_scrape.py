ea6696b9413a879bdcadf14b45df5274a693d95b
Zachary <kelvaszm@gmail.com>
2023-04-12 01:46:00 +0000

    initial fangraph_scrape.py

---
+++ fangraph_scrape.py
@@ -0,0 +1,43 @@
+#fangraph_scrape.py
+
+class fangraph_scrape:
+    """
+    fangraph_scrape class uses selenium and beautiful soup to scrape
+    mlb data from fangraph.com
+    """
+    def __init__(self):
+        pass
+
+    def get_driver(self):
+        """
+        Install the chrome webdriver and load options
+        """
+        pass
+    
+    def get_soup_page(self, url):
+        """
+        Use the webdriver and requests to get the beautiful soup page.
+        """
+        pass
+
+    def team(self):
+        """
+        Scrape the mlb teams dataset.
+        """
+        pass
+
+
+    def roster(self):
+        """
+        Scrape the roster for each mlb team.
+        """
+        pass
+
+
+    def get_href(self):
+        """
+        Scrape h_refs from page.
+        """
+        pass
+
+
