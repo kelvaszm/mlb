e6c15140b72c24f87bcb06e8d368acf1eee141f7
Zachary Kelvas <kelvaszm@gmail.com>
2023-04-15 18:46:42 +0000

    added team and get_href methods

--- fangraph_scrape.py
+++ fangraph_scrape.py
@@ -41,15 +41,34 @@ class Fangraph_scrape:
         """
         self.driver.get(url)
         page = requests.get(url)
-        
+
         return BeautifulSoup(page.content, "html.parser")
 
 
-    def team(self):
+    def team(self, soup_page):
         """
         Scrape the mlb teams dataset.
         """
-        pass
+        table_class_name = 'depth-charts-aspx_table'
+        col_list = []
+        regex = r'teamid=(\d+)'
+
+        teams_table = soup_page.find_all(class_ = table_class_name)
+        team_df = pandas.read_html(teams_table[0].prettify())[0]
+        for col in team_df.columns:
+            if col[1] != 'Team':
+                col_list.append(col[1] + ' ' + col[0])
+            
+            else:
+                col_list.append(col[1])
+
+        team_df.columns = col_list
+        
+        href_col = self.get_href(teams_table, regex)
+        
+        team_df.insert(0, 'Team Id', href_col)
+        
+        return team_df
 
 
     def roster(self):
@@ -59,10 +78,24 @@ class Fangraph_scrape:
         pass
 
 
-    def get_href(self):
+    def get_href(self, href_soup,  regex):
         """
-        Scrape h_refs from page.
+        Scrape hrefs from page.
         """
-        pass
+        href_list = []
+        for href in href_soup:
+
+            a_tags = href.find_all('a', href=True)
+
+            for tag in a_tags:
+                ids = re.findall(regex , str(tag))
+                if len(ids):
+                    href_list.append(ids[0])
+
+        return pandas.Series(href_list).drop_duplicates()
+
+
+    def __del__(self):
+        self.driver.quit()
 
 
