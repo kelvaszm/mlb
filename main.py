4d1d1f7e4bc2857f5265f7c8f354ec60dc66a648
Zachary Kelvas <kelvaszm@gmail.com>
2023-04-20 21:38:45 +0000

    added fangraph and team method

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 #main.py
-def main():
-    pass
+from fangraph_scrape import Fangraph_scrape
 
+def main():
+    try:
+        
+        #fangraphs urls
+        TEAMS_URL = 'https://www.fangraphs.com/depthcharts.aspx?position=Standings'
+        
+        #fangraph object will handle beautiful soup and selenium for web scrapping.
+        fangraph = Fangraph_scrape()
+
+        #scrape the teams page and load into dataframe
+        team_soup = fangraph.get_soup_page(TEAMS_URL)
+        team_df = fangraph.team(team_soup)
+
+    except Exception as e:
+        print(e)
 
 if __name__ == '__main__':
     main()
