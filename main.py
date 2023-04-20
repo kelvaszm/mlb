3537621a4ef2f8c3c57458b5ecaec4c2f4dd6976
Zachary Kelvas <kelvaszm@gmail.com>
2023-04-20 21:39:39 +0000

    added roster list

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def main():
         
         #fangraphs urls
         TEAMS_URL = 'https://www.fangraphs.com/depthcharts.aspx?position=Standings'
+        ROSTER_URL = 'https://www.fangraphs.com/teams/mets'
         
         #fangraph object will handle beautiful soup and selenium for web scrapping.
         fangraph = Fangraph_scrape()
@@ -13,6 +14,10 @@ def main():
         #scrape the teams page and load into dataframe
         team_soup = fangraph.get_soup_page(TEAMS_URL)
         team_df = fangraph.team(team_soup)
+
+        #scrape the player rosters from each team and load into dataframe
+        roster_soup = fangraph.get_soup_page(ROSTER_URL)
+        roster_list = fangraph.roster(roster_soup)
 
     except Exception as e:
         print(e)
