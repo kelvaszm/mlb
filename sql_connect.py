3036e87c72f4cec76a7a752217294c9b0331b8cc
Zachary Kelvas <kelvaszm@gmail.com>
2023-04-30 00:05:37 +0000

    sql class to handle mysql connection

---
+++ sql_connect.py
@@ -0,0 +1,36 @@
+#sql_connect.py
+import sqlalchemy
+
+class Sql_connect:
+    """
+    sql_connect class will create an sql alchemy object to handle statements.
+    """
+    def __init__(self, conn_string):
+        #create sql_alchemy engine
+        self.engine = self.get_engine(conn_string)
+
+
+    def get_engine(self, conn_string):
+        """
+        Create sqlaclchemy engine with connection string
+        """
+        return sqlalchemy.create_engine(conn_string, echo=False)
+
+
+    def get_column_datatypes(self, table_name):
+        """
+        Get column names and datatypes for a table.
+        """
+        md = sqlalchemy.MetaData()
+
+        table = sqlalchemy.Table(table_name, md, autoload_with=self.engine)
+
+        return [(col.name, col.type) for col in table.c]
+
+
+    def load(self, df, table_name):
+        """
+        Load pandas dataframe into database
+        """
+        return df.to_sql(name = table_name, con=self.engine, if_exists = 'append', index = False)
+
